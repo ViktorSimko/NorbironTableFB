9e76e1d6491bfaedcf5bb443cf13a362e1b2bc77
Viktor Simkó <aqviktor@gmail.com>
2016-10-16 11:14:23 +0200

    now saveing nodes to remote

---
+++ app/src/main/java/batfai/samuentropy/brainboard7/NodeProxy.java
@@ -0,0 +1,35 @@
+package batfai.samuentropy.brainboard7;
+
+/**
+ * Created by simkoviktor on 2016. 10. 16..
+ */
+
+class NodeProxy {
+    private int type;
+    private float posX;
+    private float posY;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public float getPosX() {
+        return posX;
+    }
+
+    public void setPosX(float posX) {
+        this.posX = posX;
+    }
+
+    public float getPosY() {
+        return posY;
+    }
+
+    public void setPosY(float posY) {
+        this.posY = posY;
+    }
+}
